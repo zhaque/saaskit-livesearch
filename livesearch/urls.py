773be74612078e2f85441782b68a82bb49c56488
CrowdSense <admin@crowdsense.com>
2009-09-10 10:13:06 +0100

    adding twitter without views code

--- livesearch/urls.py
+++ livesearch/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     url(r'^results$', 'livesearch.views.results', name='web_results'),
     url(r'^images$', 'livesearch.views.image_results', name='image_results'),
     url(r'^videos$', 'livesearch.views.video_results', name='video_results'),
+    url(r'^twitter$', 'livesearch.views.twitter_results', name='twitter_results'),
     url(r'^admin$', 'livesearch.views.search_admin', name='search_administartion'),
 )
